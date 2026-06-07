2ccdaebca13117c991e099361ed24187499e21ac
Davíð Steinn Geirsson <david@dsg.is>
2026-06-07 15:11:02 +0000

    Rename launcher VS project DeusExe to launcher

--- launcher/build.sh
+++ launcher/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 MSBUILD="/mnt/c/Program Files/Microsoft Visual Studio/18/Community/MSBuild/Current/Bin/MSBuild.exe"
-"$MSBUILD" "$(wslpath -w DeusExe.sln)" -p:Configuration=Release -p:Platform=Win32 -m -verbosity:minimal -nologo
+"$MSBUILD" "$(wslpath -w launcher.sln)" -p:Configuration=Release -p:Platform=Win32 -m -verbosity:minimal -nologo
